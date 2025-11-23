0c45f38646591f40fc3d0ea7b7a9ec9481ae39fc
rayentrik44-byte <rayentrik44@gmail.com>
2025-11-23 21:47:38 +0100

    Add files via upload

---
+++ api.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify, render_template
+import mysql.connector
+
+app = Flask(__name__, template_folder="templates")
+
+db = mysql.connector.connect(
+    host="mysql.railway.internal",
+    user="root",
+    password="JHxYExZtJJlfLnSEjJCDdlWkJOkCuCYT",
+    database="railway",
+    port=3306
+)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register_serial():
+    serial = request.form.get("serial")
+
+    if not serial:
+        return jsonify({"status": "error", "message": "serial required"})
+
+    cur = db.cursor()
+    cur.execute("SELECT id FROM serials WHERE serial=%s", (serial,))
+    exists = cur.fetchone()
+
+    if exists:
+        return jsonify({"status": "exists", "message": "already exists"})
+
+    cur.execute("INSERT INTO serials (serial) VALUES (%s)", (serial,))
+    db.commit()
+
+    return jsonify({"status": "success", "message": "registered"})
